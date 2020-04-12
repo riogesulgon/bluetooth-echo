f32f38fdfd8cb9ee53d3b0634175484fbe65263a
Rio <rio.g.gesulgon@gmail.com>
2020-04-13 00:12:47 +0800

    Added button and speaker.

--- bluetooth-echo.ino
+++ bluetooth-echo.ino
@@ -1,5 +1,6 @@
 #include "Keyboard.h"
 #include "macros.h"
+#include "pitches.h"
 
 char ctrlKey = KEY_LEFT_GUI; // OSX
 char ctrlKeyWin = KEY_LEFT_CTRL; // Windows
@@ -12,6 +13,9 @@ bool startRx = true;
 bool osx = false;
 bool macroMode = false;
 int macroSize = 0;
+int button1 = 3;
+int musicPin = 5;
+int buzzer = 5;
 
 void setup() {
   Serial1.begin(9600);
@@ -19,10 +23,47 @@ void setup() {
   Keyboard.begin();
   inputString.reserve(200);
   macroSize = sizeof(macro_key) / sizeof(String);
-  Serial.print((String) "Macro size" + macroSize);
+  Serial.println((String) "Macro size" + macroSize);
+  Serial1.println("Welcome!");
+  pinMode(button1, INPUT);
+  pinMode(musicPin, OUTPUT);
+  playTone();
+}
+
+void playTone() {
+ // iterate over the notes of the melody. 
+  // Remember, the array is twice the number of notes (notes + durations)
+  for (int thisNote = 0; thisNote < notes * 2; thisNote = thisNote + 2) {
+
+    // calculates the duration of each note
+    divider = melody[thisNote + 1];
+    if (divider > 0) {
+      // regular note, just proceed
+      noteDuration = (wholenote) / divider;
+    } else if (divider < 0) {
+      // dotted notes are represented with negative durations!!
+      noteDuration = (wholenote) / abs(divider);
+      noteDuration *= 1.5; // increases the duration in half for dotted notes
+    }
+
+    // we only play the note for 90% of the duration, leaving 10% as a pause
+    tone(buzzer, melody[thisNote], noteDuration*0.9);
+
+    // Wait for the specief duration before playing the next note.
+    delay(noteDuration);
+    
+    // stop the waveform generation before the next note.
+    noTone(buzzer);
+  }
 }
 
 void loop() {
+  int buttonState = digitalRead(button1);
+  if (buttonState == HIGH) {
+    Serial.println("Button 1 pressed");
+    Serial1.println("Ok!");
+    playTone();
+  }
   while (Serial1.available() > 0){   
     char inChar = (char)Serial1.read();
     if (startRx && (inChar == '!' || inChar == '^' || inChar == '+' || inChar == '#' || inChar == '~')) {
